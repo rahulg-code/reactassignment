3f76869fdd8b2440dbb56ddafe8cf099f89b12ab
rahulgawali <rahulgawali@SDN-282>
2020-10-05 12:17:33 +0530

    Updates

---
+++ precache-manifest.afa2d3c4851bba203f53074f41768831.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351bd13aede22f226101d10415873584",
+    "url": "/reactassignment/index.html"
+  },
+  {
+    "revision": "90649571130f08b3eaba",
+    "url": "/reactassignment/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "44db5155aa59d08341a9",
+    "url": "/reactassignment/static/css/main.664a4d90.chunk.css"
+  },
+  {
+    "revision": "90649571130f08b3eaba",
+    "url": "/reactassignment/static/js/2.e14858b2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactassignment/static/js/2.e14858b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44db5155aa59d08341a9",
+    "url": "/reactassignment/static/js/main.80c153ee.chunk.js"
+  },
+  {
+    "revision": "975df7eabf85e7222a81",
+    "url": "/reactassignment/static/js/runtime-main.c8693110.js"
+  }
+]);
